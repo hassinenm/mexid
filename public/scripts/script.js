d099bac3baa2eb60b62fc448506473b5efa7d453
hassinenm <mh@live.fi>
2015-04-21 23:13:52 +0300

    Initial commit to GitHub

---
+++ public/scripts/script.js
@@ -0,0 +1,121 @@
+// create the module and name it scotchApp
+// also include ngRoute for all our routing needs
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+// configure our routes
+scotchApp.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'pages/home.html',
+            controller  : 'mainController'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'pages/about.html',
+            controller  : 'aboutController'
+        })
+        .when('/mexicanid', {
+            templateUrl : 'pages/mexicanid.html',
+            controller  : 'contactController'
+        })
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        });
+        
+});
+
+// create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+scotchApp.controller('mexicanidController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Create your own Mexican ID!';
+});
+scotchApp.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+scotchApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
+
+scotchApp.directive("idcard", function(){
+return {
+    restrict: "A",
+        link: function(scope, element){
+            var ctx = element[0].getContext('2d');
+            ctx.lineWidth = 80;
+            ctx.strokeStyle = "red";
+            ctx.beginPath();
+            ctx.moveTo(-20, 120);
+            ctx.bezierCurveTo(300, -40, 370, 80, 500, -20);
+            ctx.stroke();
+
+            ctx.lineWidth = 7;
+            ctx.strokeStyle = "white";
+            ctx.beginPath();
+            ctx.moveTo(-10, 67);
+            ctx.bezierCurveTo(400, -10, 360, 105, 500, 18);
+            ctx.stroke();
+
+            ctx.lineWidth = 70;
+            ctx.strokeStyle = "green";
+            ctx.beginPath();
+            ctx.moveTo(-20, 30);
+            ctx.bezierCurveTo(400, -50, 370, 80, 500, -30)
+            ctx.stroke();
+            
+            ctx.font = "35px Calibri";
+            ctx.fillStyle = 'white';
+            ctx.fillText("MEXICO", 70, 30);
+            
+            ctx.font = "15px Calibri";
+            ctx.fillStyle = 'white';
+            ctx.fillText("MATRICULA CONSULAR", 200, 20);
+            
+            ctx.font = "12px Calibri";
+            ctx.fillStyle = 'white';
+            ctx.fillText("CONSULAR ID CARD", 350, 20);
+            
+            ctx.font = "9px Calibri";
+            ctx.fillStyle = 'black';
+            ctx.fillText("Nombre/Name", 200, 80);
+            ctx.fillText("Lugar y Fecha de Nacimiento/Place of Birth and Birth Date", 200, 125);
+            ctx.fillText("Dirección/Address", 200, 170);
+            ctx.fillText("Fecha de Emissión/Date of Issue", 200, 215);
+            ctx.fillText("Fecha de Expiración/Date of Expiry", 325, 215);
+            ctx.fillText("Authoridad/Authority", 200, 260);
+            ctx.fillText("N° Documento/ID Number", 325, 260);
+            
+                        
+            ctx.font = "Bold 12px Calibri";
+            ctx.fillStyle = 'black';
+            ctx.fillText("MARKKU", 202, 95);
+            ctx.fillText("HASSINEN", 202, 110);
+            ctx.fillText("OAXACA", 202, 140);
+            ctx.fillText("01 DIC 1955", 202, 155);
+            ctx.fillText("OAXACA", 202, 185);
+            ctx.fillText("01 DIC 1955", 202, 200);
+            
+            var img = document.createElement("img");
+            img.src = "http://izkali.ysosdev.com/files/2011/08/Product-of-mexico-watermark_op40.png";
+            ctx.drawImage(img,250,80, 200, 200);
+            
+            var img = document.createElement("img");
+            img.src = "http://s11.postimg.org/4xzowsg8z/estados_unidos.png";
+            ctx.drawImage(img,3,1, 55, 55);
+            
+            var img = document.createElement("img");
+            img.src = "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xtp1/v/t1.0-1/c244.2.716.716/s320x320/10177890_10152582171437660_2291415937533671490_n.jpg?oh=75f954a5d005a14688af85d9c50026ad&oe=55D3D237&__gda__=1440171177_20fd24f3e3a2f495dde756ce99313ff2";
+            ctx.drawImage(img,20,80, 150, 200);
+            
+       }
+    };
+});
---
+++ server.js
@@ -0,0 +1,93 @@
+// server.js
+
+    // set up ========================
+    var express  = require('express');
+    var app      = express();                               // create our app w/ express
+    var mongoose = require('mongoose');                     // mongoose for mongodb
+    var morgan = require('morgan');             // log requests to the console (express4)
+    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+    // configuration =================
+
+    // mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+    mongoose.connect('mongodb://localhost/mexid');
+
+    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+
+    // define model =================
+    var schema = new mongoose.Schema({
+	forename: 'String',
+	surname: 'String',
+                                     });
+    var Mexican = mongoose.model('Mexican', schema);
+
+
+    // routes ======================================================================
+
+    // api ---------------------------------------------------------------------
+    // get all todos
+    app.get('/api/mexicans', function(req, res) {
+
+        // use mongoose to get all todos in the database
+        Mexican.find(function(err, mexicans) {
+
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+
+            res.json(mexicans); // return all mexicans in JSON format
+        });
+    });
+
+    // create mexican and send back all mexicans after creation
+    app.post('/api/mexicans', function(req, res) {
+
+        // create a mexican, information comes from AJAX request from Angular
+        Mexican.create({
+            text : req.body.text,
+            done : false
+        }, function(err, mexican) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Mexican.find(function(err, mexicans) {
+                if (err)
+                    res.send(err)
+                res.json(mexicans);
+            });
+        });
+
+    });
+
+    // delete a mexican
+    app.delete('/api/mexicans/:mexican_id', function(req, res) {
+        Mexican.remove({
+            _id : req.params.mexican_id
+        }, function(err, mexican) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Mexican.find(function(err, mexicans) {
+                if (err)
+                    res.send(err)
+                res.json(mexicans);
+            });
+        });
+    });
+
+    // application -------------------------------------------------------------
+    app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+
+    // listen (start app with node server.js) ======================================
+    app.listen(8080);
+    console.log("App listening on port 8080");
