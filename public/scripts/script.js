77f71597e5fd841ae7a512292e83041d2ec5a31b
hassinenm <mh@live.fi>
2015-04-22 15:30:24 +0300

    Cleaned unnecesary routes and controllers

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -8,19 +8,10 @@ scotchApp.config(function($routeProvider) {
 
         // route for the home page
         .when('/', {
-            templateUrl : 'pages/home.html',
-            controller  : 'mainController'
+            templateUrl : 'pages/mexicanid.html',
+            controller  : 'mexicanidController'
         })
 
-        // route for the about page
-        .when('/about', {
-            templateUrl : 'pages/about.html',
-            controller  : 'aboutController'
-        })
-        .when('/mexicanid', {
-            templateUrl : 'pages/mexicanid.html',
-            controller  : 'contactController'
-        })
         // route for the contact page
         .when('/contact', {
             templateUrl : 'pages/contact.html',
@@ -30,20 +21,13 @@ scotchApp.config(function($routeProvider) {
 });
 
 // create the controller and inject Angular's $scope
-scotchApp.controller('mainController', function($scope) {
-    // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
-});
 scotchApp.controller('mexicanidController', function($scope) {
     // create a message to display in our view
     $scope.message = 'Create your own Mexican ID!';
 });
-scotchApp.controller('aboutController', function($scope) {
-    $scope.message = 'Look! I am an about page.';
-});
 
 scotchApp.controller('contactController', function($scope) {
-    $scope.message = 'Contact us! JK. This is just a demo.';
+    $scope.message = 'You can also contact me!';
 });
 
 scotchApp.directive("idcard", function(){
